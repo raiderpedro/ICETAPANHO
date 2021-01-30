f1e11444a432633d03db549fd4d9924167b78d9d
raiderpedro <76105316+raiderpedro@users.noreply.github.com>
2021-01-30 20:11:46 +0000

    update worldgenerator

---
+++ ICETAPANHO_Project/Assets/wolrdGeneratorController/worldGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class worldGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+public const int chunkLenght = 20;     
+public Tilemap[] chunk = new Tilemap[3];     
+private int posUltimo;     
+private int limite = 10, i = 0;
+
+void Update()
+    {
+
+        while (i < limite)
+        {
+            int chunkId = Random.Range(0, chunk.Length);
+            Tilemap objeto;
+
+            objeto = Instantiate(chunk[chunkId], new Vector3(posUltimo, 0, 0), Quaternion.identity);
+
+            objeto.transform.parent = this.transform;
+
+            posUltimo += chunkLenght;
+
+            i++;
+        }
+
+    }
+
+}
